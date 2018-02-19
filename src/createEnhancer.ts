0ba79a07e247d126f1ae3bef8aca3675defb5c7d
Vadim Dalecky <streamich@gmail.com>
2018-02-19 23:34:16 +0000

    feat: add enchancer display name in dev

--- src/createEnhancer.ts
+++ src/createEnhancer.ts
@@ -10,20 +10,24 @@ export const divWrapper = (Comp, propName, props, state) =>
   h('div', null, noWrap(Comp, propName, props, state)) as any;
 
 const createEnhancer = (Facc, prop?: string, wrapper = noWrap) => {
-    const hoc = (Comp, propName: any = prop, faccProps: object = null) => {
+    const enhancer = (Comp, propName: any = prop, faccProps: object = null) => {
         const isClassDecoratorMethodCall = typeof Comp === 'string';
 
         if (isClassDecoratorMethodCall) {
-            return (Klass) => hoc(Klass, Comp as any || prop, propName as any);
+            return (Klass) => enhancer(Klass, Comp as any || prop, propName as any);
         }
 
         const Enhanced = (props) =>
             h(Facc, faccProps, (state) => wrapper(Comp, propName, props, state));
 
+        if (process.env.NODE_ENV !== 'production') {
+            (Facc as any).displayName = `${Facc.displayName || Facc.name}(${Comp.displayName || Comp.name})`;
+        }
+
         return isClassDecoratorMethodCall ? addClassDecoratorSupport(Enhanced) : Enhanced;
     };
 
-    return hoc;
+    return enhancer;
 }
 
 export default createEnhancer;
