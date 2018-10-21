81e43aa4f3218c425c1dc34a51e8b0f5f7bac32c
streamich <streamich@gmail.com>
2018-10-21 13:26:08 +0200

    feat: 🎸 add universal component prop type interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import render from './render';
 import createEnhancer from './createEnhancer';
 
+export interface UniversalProps<Data> {
+    children?: ((data: Data) => React.ReactNode) | React.ReactNode;
+    render?: (data: Data) => React.ReactNode;
+    comp?: React.ComponentType<Data & any>;
+    component?: React.ComponentType<Data & any>;
+}
+
 export {
     render,
     createEnhancer,
